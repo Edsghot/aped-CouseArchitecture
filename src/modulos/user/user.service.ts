297441abf9fcac7bc22ad27724e5b5ff7ada3ac1
edsghot <edsghot@gmail.com>
2024-06-16 21:15:47 -0500

    traduciendo a español

--- src/modulos/user/user.service.ts
+++ src/modulos/user/user.service.ts
@@ -20,33 +20,37 @@ export class UserService {
 
   async insertUser(request: CreateUserDto) {
     try {
-        let band: { success: boolean, msg: string };
-        band = await this.validateService.validateDni(request.Dni);
-        
-        if(!band.success){
-            return {msg: band.msg,success: band.success, data:null}
-        }
-        band = await this.validateService.validatePhoneNumber(request.PhoneNumber);
-        
-        if(!band.success){
-            return {msg: band.msg,success: band.success, data:null}
-        }
-        band = await this.validateService.validateFirstName(request.FirstName);
-        
-        if(!band.success){
-            return {msg: band.msg,success: band.success, data:null}
-        }
-        band = await this.validateService.validateLastName(request.LastName);
-
-        if(!band.success){
-            return {msg: band.msg,success: band.success, data:null}
-        }
-
-        const userDni = await this.userRepository.findOne({where: {Dni: request.Dni}})
-
-        if(userDni){
-            return {msg: "ya se registro un usuario con ese Dni",success: false, data:null}
-        }
+      let band: { success: boolean, msg: string };
+      band = await this.validateService.validateDni(request.Dni);
+      
+      if (!band.success) {
+        return { msg: band.msg, success: band.success, data: null };
+      }
+  
+      band = await this.validateService.validatePhoneNumber(request.PhoneNumber);
+      
+      if (!band.success) {
+        return { msg: band.msg, success: band.success, data: null };
+      }
+  
+      band = await this.validateService.validateFirstName(request.FirstName);
+      
+      if (!band.success) {
+        return { msg: band.msg, success: band.success, data: null };
+      }
+  
+      band = await this.validateService.validateLastName(request.LastName);
+  
+      if (!band.success) {
+        return { msg: band.msg, success: band.success, data: null };
+      }
+  
+      const userDni = await this.userRepository.findOne({ where: { Dni: request.Dni } });
+  
+      if (userDni) {
+        return { msg: "Ya se registró un usuario con ese DNI", success: false, data: null };
+      }
+  
       const newUser = this.userRepository.create({
         FirstName: request.FirstName,
         Sex: request.Sex,
@@ -58,25 +62,26 @@ export class UserService {
         Mail: request.Mail,
         Rol: request.Rol,
       });
-
+  
       // Guardar la nueva entidad de usuario en la base de datos
       await this.userRepository.save(newUser);
-
-      return { msg: 'User inserted successfully', success: true };
+  
+      return { msg: 'Usuario insertado exitosamente', success: true };
     } catch (error) {
-      console.error('Failed to insert user:', error);
-      return { msg: 'Failed to insert user', detailMsg: error, success: false };
+      console.error('Error al insertar usuario:', error);
+      return { msg: 'Error al insertar usuario', detailMsg: error, success: false };
     }
   }
+  
   async updateUser(updateUserDto: UpdateUserDto) {
     try {
       const user = await this.userRepository.findOne({
         where: { IdUser: updateUserDto.IdUser },
       });
       if (!user) {
-        return { msg: 'User not found', success: false };
+        return { msg: 'Usuario no encontrado', success: false };
       }
-
+  
       user.FirstName = updateUserDto.FirstName;
       user.LastName = updateUserDto.LastName;
       user.Password = updateUserDto.Password;
@@ -86,71 +91,72 @@ export class UserService {
       user.Sex = updateUserDto.Sex;
       user.Mail = updateUserDto.Mail;
       user.Rol = updateUserDto.Rol;
-
+  
       await this.userRepository.save(user);
-
-      return { msg: 'User updated successfully', success: true };
+  
+      return { msg: 'Usuario actualizado exitosamente', success: true };
     } catch (error) {
-      return { msg: 'Failed to update user', detailMsg: error, success: false };
+      return { msg: 'Error al actualizar usuario', detailMsg: error, success: false };
     }
   }
-
+  
   async getAllUsers() {
     try {
       const users = await this.userRepository.find();
-      return { data: users, msg: 'Success', success: true };
+      return { data: users, msg: 'Éxito', success: true };
     } catch (error) {
-      console.error('Failed to get users:', error);
-      return { msg: 'Failed to get users', detailMsg: error, success: false };
+      console.error('Error al obtener usuarios:', error);
+      return { msg: 'Error al obtener usuarios', detailMsg: error, success: false };
     }
   }
-
+  
   async deleteUser(userId: number) {
     try {
       await this.userRepository.delete(userId);
-      return { msg: 'User deleted successfully', success: true };
+      return { msg: 'Usuario eliminado exitosamente', success: true };
     } catch (error) {
-      console.error('Failed to delete user:', error);
-      return { msg: 'Failed to delete user', detailMsg: error, success: false };
+      console.error('Error al eliminar usuario:', error);
+      return { msg: 'Error al eliminar usuario', detailMsg: error, success: false };
     }
   }
-
+  
   async getUserById(userId: number) {
     try {
       const user = await this.userRepository.findOne({
         where: { IdUser: userId },
       });
-      return { data: user, msg: 'Success', success: true };
+      return { data: user, msg: 'Éxito', success: true };
     } catch (error) {
-      console.error('Failed to get user by ID:', error);
-      return { msg: 'Failed to get user', detailMsg: error, success: false };
+      console.error('Error al obtener usuario por ID:', error);
+      return { msg: 'Error al obtener usuario', detailMsg: error, success: false };
     }
   }
-
+  
   async login(userRequest: string, password: string) {
     try {
       let userRes;
       userRes = await this.userRepository.findOne({
         where: { Mail: userRequest, Password: password },
       });
-
+  
       if (!userRes) {
         userRes = await this.userRepository.findOne({
           where: { Dni: userRequest, Password: password },
         });
-
+  
         if (!userRes) {
           return {
             data: null,
-            msg: 'Invalid username or password',
+            msg: 'Usuario o contraseña incorrectos',
             success: false,
           };
         }
       }
-
-      return { data: userRes, msg: 'Success', success: true };
+  
+      return { data: userRes, msg: 'Éxito', success: true };
     } catch (error) {
-      return { msg: 'Login failed', detailMsg: error, success: false };
+      return { msg: 'Error al iniciar sesión', detailMsg: error, success: false };
     }
   }
+  
 }
