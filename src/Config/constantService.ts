2a3e6e16927c718a6beace9d0701d30dcf7c0bf1
edsghot <edsghot@gmail.com>
2024-07-16 00:28:01 -0500

    se cambio el correo

--- src/Config/constantService.ts
+++ src/Config/constantService.ts
@@ -1,4 +1,4 @@
 export const CLOUDINARY = 'Cloudinary'
 export const FOLDER_PAYMENT = 'Xnor/Payment'
 export const CHAT_USER = 'Chat/User'
-export const CORREO = 'edsghotsolutions@gmail.com'
+export const CORREO = 'cochadeu@gmail.com'
