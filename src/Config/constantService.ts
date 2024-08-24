6729527f9578faaebaa27b6f39f5851847e4049d
edsghot <edsghot@gmail.com>
2024-08-24 17:16:12 -0500

    se cambio el correo

--- src/Config/constantService.ts
+++ src/Config/constantService.ts
@@ -1,4 +1,4 @@
 export const CLOUDINARY = 'Cloudinary'
 export const FOLDER_PAYMENT = 'Xnor/Payment'
 export const CHAT_USER = 'Chat/User'
-export const CORREO = 'cochadeu@gmail.com'
+export const CORREO = '191243@unamba.edu.pe'
